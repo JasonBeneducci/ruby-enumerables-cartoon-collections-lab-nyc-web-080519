4b6a68429c188e2ae2296b299997907ca1ba2c3d
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-08-01 16:51:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |word|
-    if word.length > 4
+  new_array = array.select {|word| word.length > 4}
+    if new_array.length > 0
       puts "true"
     end
   end
-  puts "false"
 end
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
